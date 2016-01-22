08d900bf3b4b86a97e1b3939b6ca4bce04d5c1a1
Fredrik Kloster <fredrik_kloster@hotmail.com>
2016-01-22 13:43:46 +0100

    Added exceptions

--- src/main/java/com/guthub/fredrikzkl/furyracers/game/WebsocketClient.java
+++ src/main/java/com/guthub/fredrikzkl/furyracers/game/WebsocketClient.java
@@ -2,16 +2,19 @@ package com.guthub.fredrikzkl.furyracers.game;
 
 
 import javax.websocket.*;
+
+import com.github.fredrikzkl.furyracers.Furyracers.App;
+
 import java.io.IOException;
 
 public class WebsocketClient {
 	//Istedet for app, skal det være vår "application" klasse
 	private GameSession gameSession = App.getGameSession();
 	
-	public void onOpen(Session session){
+	public void onOpen(Session session) throws IOException, EncodeException{
 		gameSession.onOpen(session);
 	}
-	public void onMessage(Session session, String message){
+	public void onMessage(Session session, String message) throws IOException, EncodeException{
 		gameSession.onMessage(session,message);
 	}
 	
