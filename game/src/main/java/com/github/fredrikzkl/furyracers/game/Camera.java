0d5451c10d3e5ae51660ebc47269ee76f63fd54e
Fredrik Kloster <fredrik_kloster@hotmail.com>
2016-02-09 11:53:01 +0100

    Fixed camera + map

--- game/src/main/java/com/github/fredrikzkl/furyracers/game/Camera.java
+++ game/src/main/java/com/github/fredrikzkl/furyracers/game/Camera.java
@@ -2,6 +2,8 @@ package com.github.fredrikzkl.furyracers.game;
 
 import org.newdawn.slick.Graphics;
 
+import com.github.fredrikzkl.furyracers.Application;
+
 
 
 
@@ -17,8 +19,9 @@ public class Camera {
 	}
 	
 	public void update(float posX, float posY) {
-		x = -posX;
+		x = -posX / (float) Application.screenSize.getWidth()/2;
 		y = -posY;
+		
 		
 		if(posX<0)
 			x = 0;
@@ -26,10 +29,11 @@ public class Camera {
 		if(posY <0)
 			y = 0;
 		
-	
-		//level.map.getWidth()*level.map.getTileWidth()
+		if(-x > (level.map.getWidth()*level.map.getTileWidth() - Application.screenSize.getWidth()))
+			x = (float) -((level.map.getWidth()*level.map.getTileWidth() - Application.screenSize.getWidth()));
 		
-	
+		if(-y > (level.map.getHeight()*level.map.getTileHeight() - Application.screenSize.getHeight()))
+			y = (float) -((level.map.getHeight()*level.map.getTileHeight() - Application.screenSize.getHeight()));
 		
 	}
 
