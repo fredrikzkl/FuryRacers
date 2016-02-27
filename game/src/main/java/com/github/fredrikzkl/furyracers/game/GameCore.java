ab34f51ec2cea842366611daf871418ad7720ad5
Fredrik Kloster <fredrik_kloster@hotmail.com>
2016-02-27 21:18:50 +0100

    ScoreBoard class is ready

--- game/src/main/java/com/github/fredrikzkl/furyracers/game/GameCore.java
+++ game/src/main/java/com/github/fredrikzkl/furyracers/game/GameCore.java
@@ -24,79 +24,63 @@ import com.github.fredrikzkl.furyracers.Application;
 import com.github.fredrikzkl.furyracers.network.GameSession;
 
 public class GameCore extends BasicGameState {
-	
+
 	private String IP = "";
-	
-	private final int menuID = 0; 
-	
+
+	private final int menuID = 0;
 
 	public static Camera camera;
 	public Level level = null;
 
 	public float initalZoom, zoom = 1;
-	
+
 	Font font;
 	TrueTypeFont ttf;
 	Image subMapPic, mapPic;
-	
+
 	public List<Car> cars;
 	public List<Player> players;
 
 	private int cameraMargin = 250, screenWidth, screenHeight;
-	
+
 	float biggest = 0;
-	
+
 	private boolean keyboardPlayerOne, keyboardPlayerTwo;
 
-	private long startTimeCountdown, nanoSecondsElapsed, 
-				 currentTimeCountDown, secondsElapsed;
+	private long startTimeCountdown, nanoSecondsElapsed, currentTimeCountDown, secondsElapsed;
 
-	private boolean raceStarted, countdownStarted,
-					startGoSignal, goSignal;
+	private boolean raceStarted, countdownStarted, startGoSignal, goSignal;
 
 	private long startGoSignalTime, goSignalTimeElapsed, secondsLeft;
-	
-	
-	private boolean raceFinished = false;
-	private Image results, highscores;
-	float resultPosX, resultPosY, highScorePosX, highScorePosY;
-	private Color headerColor = new Color(221, 0, 0);
-	private TrueTypeFont scoreBoardHeader;
-	private TrueTypeFont scoreBoardText;
-	private float headerSize,textSize;
-	private int textTimer = 0;
-	int scoreBoardLength;
-	
 
+	private boolean raceFinished = false;
 	private TrueTypeFont countDownFont;
 
-	private boolean toMenuTimerSet,returnToMenu = false;
-	private long toMenuRealTimer;
-	private String toMenuTimer = "";
-	private int timeToMenu = 15;
+	private ScoreBoard scoreboard;
 
 	private static Sound drivingSound;
 
 	public void init(GameContainer container, StateBasedGame sbg) throws SlickException {
 		System.out.println("IP: " + IP);
 	}
-	
-	public void gameStart(int levelNr, List<Player> players) throws SlickException{
+
+	public void gameStart(int levelNr, List<Player> players) throws SlickException {
 
 		initSounds();
 		GameSession.setGameState(getID());
 		Application.setInMenu(false);
 		level = new Level(levelNr);
-		camera = new Camera(0,0,level);
+		camera = new Camera(0, 0, level);
 		initVariables();
 		createPlayers(players);
-		camera.setZoom((float)0.3);
+		camera.setZoom((float) 0.3);
 		
+		scoreboard = new ScoreBoard(cars, players);
 		addFonts();
 	}
 
-	public void update(GameContainer container , StateBasedGame game, int deltaTime) throws SlickException {
-		
+	public void update(GameContainer container, StateBasedGame game, int deltaTime) throws SlickException {
+
 		checkForKeyboardInput(container, game);
 		startCountdown();
 		checkCountdown();
@@ -104,33 +88,33 @@ public class GameCore extends BasicGameState {
 		checkDistances();
 		camera.zoomLogic();
 		camera.updateCamCoordinates();
-		
+
 	}
 
-	public void render(GameContainer container, StateBasedGame sbg, Graphics g)
-			throws SlickException {
-		
+	public void render(GameContainer container, StateBasedGame sbg, Graphics g) throws SlickException {
+
 		relocateCam(g);
 		drawCarTimes();
 		drawCountdown(g);
-		ttf.drawString(screenWidth-300, 10, IP);//Ip addresene øverst i venstre corner
-		
-		if(raceFinished)
-		drawScoreBoard();
-		if(returnToMenu)
+		ttf.drawString(screenWidth - 300, 10, IP);// Ip addresene øverst i
+													// venstre corner
+
+		if (raceFinished)
+			scoreboard.drawScoreBoard();
+		if (scoreboard.isReturnToMenuTimerDone())
 			returnToMenu(container, sbg);
+
 	}
-	
-	
-	public void createPlayers(List<Player> players) throws SlickException{
-		
+
+	public void createPlayers(List<Player> players) throws SlickException {
+
 		this.players = players;
-		
-		for(Player player:players){
-			createPlayer(player.getPlayerNr(),player.getId(), player.getSelect());
+
+		for (Player player : players) {
+			createPlayer(player.getPlayerNr(), player.getId(), player.getSelect());
 		}
 	}
-	
+
 	public void addFonts() {
 
 		InputStream inputStream;
@@ -141,238 +125,207 @@ public class GameCore extends BasicGameState {
 			Font timberFont = Font.createFont(Font.TRUETYPE_FONT, inputStream);
 
 			timberFont = timberFont.deriveFont(countdownFontSize);
-			
+
 			countDownFont = new TrueTypeFont(timberFont, true);
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void startCountdown(){
-		
-		if(!countdownStarted){
+
+	public void startCountdown() {
+
+		if (!countdownStarted) {
 			startTimeCountdown = System.nanoTime();
 			countdownStarted = true;
 		}
 	}
-	
-	public void drawCountdown(Graphics g){
-		
+
+	public void drawCountdown(Graphics g) {
+
 		Color countdownColor = new Color(221, 0, 0);
-		
-		if(!raceStarted){
-			countDownFont.drawString(screenWidth/2, screenHeight/2-150, "" + secondsLeft, countdownColor);
-		}else if(startGoSignal){
+
+		if (!raceStarted) {
+			countDownFont.drawString(screenWidth / 2, screenHeight / 2 - 150, "" + secondsLeft, countdownColor);
+		} else if (startGoSignal) {
 			startGoSignalTime = System.currentTimeMillis();
 			startGoSignal = false;
 			goSignal = true;
 		}
 
-		if(goSignal){
+		if (goSignal) {
 			long currentTime = System.currentTimeMillis();
 			goSignalTimeElapsed = currentTime - startGoSignalTime;
-			if(goSignalTimeElapsed < 1500){
-				countDownFont.drawString(screenWidth/2-50, screenHeight/2-150, "RACE!", countdownColor);
-			}else{
+			if (goSignalTimeElapsed < 1500) {
+				countDownFont.drawString(screenWidth / 2 - 50, screenHeight / 2 - 150, "RACE!", countdownColor);
+			} else {
 				goSignal = false;
 			}
 		}
 	}
-	
-	public void drawCarTimes(){
-		
-		for(int i = 0; i < cars.size(); i++){
-			
+
+	public void drawCarTimes() {
+
+		for (int i = 0; i < cars.size(); i++) {
+
 			float yOffSet = 20;
-			float yNextPlayerOffSet = yOffSet*4;
-			float startY = screenHeight/10 + (yNextPlayerOffSet*i);
-			float startX = screenWidth/40;
-			
-			ttf.drawString(startX, startY, "Player" + cars.get(i).getPlayerNr()+":");
-			ttf.drawString(startX, startY + yOffSet,"Lap "  + cars.get(i).getLaps() + "/3");
-			ttf.drawString(startX, startY + yOffSet*2, ""+cars.get(i).getTimeElapsed());
+			float yNextPlayerOffSet = yOffSet * 4;
+			float startY = screenHeight / 10 + (yNextPlayerOffSet * i);
+			float startX = screenWidth / 40;
+
+			ttf.drawString(startX, startY, "Player" + cars.get(i).getPlayerNr() + ":");
+			ttf.drawString(startX, startY + yOffSet, "Lap " + cars.get(i).getLaps() + "/3");
+			ttf.drawString(startX, startY + yOffSet * 2, "" + cars.get(i).getTimeElapsed());
 		}
 	}
-	
-	public void updateCars(GameContainer container, StateBasedGame game, int deltaTime) throws SlickException{
-		
+
+	public void updateCars(GameContainer container, StateBasedGame game, int deltaTime) throws SlickException {
+
 		int carsFinished = 0;
-		
-		for(Car car: cars){
+
+		for (Car car : cars) {
 			car.update(container, game, deltaTime);
-			if(car.finishedRace())
+			if (car.finishedRace())
 				carsFinished++;
 		}
-		
-		if(carsFinished == cars.size()){
+
+		if (carsFinished == cars.size()) {
 			raceFinished = true;
-			//returnToMenu(container , game);
+			// returnToMenu(container , game);
 		}
 	}
-	
-	private void drawScoreBoard() {
-		float speed = 1;
-		float maxX = Application.screenSize.width;
-		float midWay = maxX/2;
-		float marginX = midWay/5;
-		float scalingValue = (marginX*3)/results.getWidth();
-		
-		results.draw(resultPosX-results.getWidth(),resultPosY,scalingValue);
-		highscores.draw(highScorePosX+highscores.getWidth(),highScorePosY,scalingValue);
-		
-		if(resultPosX<marginX*3.5 || highScorePosX>(midWay-(marginX*1.3))){
-			resultPosX += speed;
-			highScorePosX -= speed;
-		}else{
-			printScores(resultPosX - results.getWidth());
-			printHighScores(highScorePosX +  results.getWidth());
-			printTimer(maxX);
-		}
-			
-		
-		
-	}
-	
-	
+
 	private void initSounds() {
 		try {
 			String path = "Sound/";
 			drivingSound = new Sound(path + "driving.wav");
-			
+
 		} catch (SlickException e) {
 			System.out.println("Could not load sound file" + e);
 			e.printStackTrace();
 		}
 	}
-	
-	public static Sound getDrivingSound(){
-		return drivingSound;
-	}
-	
-	public void returnToMenu(GameContainer container, StateBasedGame game) throws SlickException{
+
+	public void returnToMenu(GameContainer container, StateBasedGame game) throws SlickException {
 		Application.closeConnection();
 		Application.createGameSession();
 		game.getState(menuID).init(container, game);
 		game.enterState(menuID);
 	}
-	
-	public void checkCountdown(){
-		if(!raceStarted){
+
+	public void checkCountdown() {
+		if (!raceStarted) {
 			currentTimeCountDown = System.nanoTime();
 			nanoSecondsElapsed = currentTimeCountDown - startTimeCountdown;
 			secondsElapsed = TimeUnit.NANOSECONDS.toSeconds(nanoSecondsElapsed);
 			secondsLeft = 3 - secondsElapsed;
-			
-			if(secondsLeft == 0){
+
+			if (secondsLeft == 0) {
 				startRace();
 			}
 		}
 	}
-	
-	public void startRace(){
-		
-		for(Car car : cars)
+
+	public void startRace() {
+		for (Car car : cars)
 			car.startClock();
-		
 		raceStarted = true;
 	}
-	
+
 	private void checkDistances() {
-		
+
 		Vector2f longestDistance = new Vector2f();
 		Vector2f shortestDistance = new Vector2f(level.getMapWidthPixels(), level.getMapHeightPixels());
 		Vector2f closestEdge = new Vector2f(level.getMapWidthPixels(), level.getMapHeightPixels());
-		
-		for(Car car : cars){
-			if(car.position.x >longestDistance.x){
+
+		for (Car car : cars) {
+			if (car.position.x > longestDistance.x) {
 				longestDistance.x = car.position.x;
 			}
-			if(car.position.x < shortestDistance.x){
+			if (car.position.x < shortestDistance.x) {
 				shortestDistance.x = car.position.x;
 			}
-			if(car.position.y > longestDistance.y){
+			if (car.position.y > longestDistance.y) {
 				longestDistance.y = car.position.y;
- 			}
-			if(car.position.y < shortestDistance.y){
+			}
+			if (car.position.y < shortestDistance.y) {
 				shortestDistance.y = car.position.y;
 			}
-			
-			if(car.position.x < closestEdge.x){
+
+			if (car.position.x < closestEdge.x) {
 				closestEdge.x = car.position.x;
 			}
-			if(car.position.y < closestEdge.y){
+			if (car.position.y < closestEdge.y) {
 				closestEdge.y = car.position.y;
 			}
-				
+
 		}
-		
+
 		Vector2f deltaDistance = new Vector2f();
-		
+
 		deltaDistance.x = longestDistance.x - shortestDistance.x;
 		deltaDistance.y = longestDistance.y - shortestDistance.y;
-		
+
 		camera.setDeltaDistances(deltaDistance);
 		camera.setClosestEdge(closestEdge);
 	}
-	
-	public void checkForKeyboardInput(GameContainer container, StateBasedGame game) throws SlickException{
+
+	public void checkForKeyboardInput(GameContainer container, StateBasedGame game) throws SlickException {
 		Input input = container.getInput();
-		/*if(input.isKeyDown(Input.KEY_A) && !keyboardPlayerOne){
-			int amountOfPlayers = cars.size();
-			createPlayer(amountOfPlayers+1,"keyboardPlayer",1);
-			cars.get(amountOfPlayers).activateKeyboardInput();
-			keyboardPlayerOne = true;
+		/*
+		 * if(input.isKeyDown(Input.KEY_A) && !keyboardPlayerOne){ int
+		 * amountOfPlayers = cars.size();
+		 * createPlayer(amountOfPlayers+1,"keyboardPlayer",1);
+		 * cars.get(amountOfPlayers).activateKeyboardInput(); keyboardPlayerOne
+		 * = true; }
+		 * 
+		 * if(input.isKeyDown(Input.KEY_B) && !keyboardPlayerTwo){ int
+		 * amountOfPlayers = cars.size();
+		 * createPlayer(amountOfPlayers+1,"keyboardPlayerTwo",1);
+		 * cars.get(amountOfPlayers).activateKeyboardInput(); keyboardPlayerTwo
+		 * = true; }
+		 */
+
+		if (input.isKeyPressed(Input.KEY_R)) {
+			returnToMenu(container, game);
 		}
-		
-		if(input.isKeyDown(Input.KEY_B) && !keyboardPlayerTwo){
-			int amountOfPlayers = cars.size();
-			createPlayer(amountOfPlayers+1,"keyboardPlayerTwo",1);
-			cars.get(amountOfPlayers).activateKeyboardInput();
-			keyboardPlayerTwo = true;
-		}*/
-		
-	    if(input.isKeyPressed(Input.KEY_R)){
-	    	returnToMenu(container, game);
-	    }
-	    
+
 	}
-	
-	public void createPlayer(int nr, String id, int playerChoice) throws SlickException{
-		
+
+	public void createPlayer(int nr, String id, int playerChoice) throws SlickException {
+
 		CarProperties temp = CarProperties.values()[playerChoice];
-		
-		cars.add(new Car(temp,id,nr,
-				level.getStartCoordinates().x-(level.tileWidth*4),
-				level.getStartCoordinates().y-(level.tileHeight*4),
-				level));
+
+		cars.add(new Car(temp, id, nr, level.getStartCoordinates().x - (level.tileWidth * 4),
+				level.getStartCoordinates().y - (level.tileHeight * 4), level));
 	}
-	
-	public void relocateCam(Graphics g){
-		camera.zoom(g, camera.getZoom());//Crasher om verdien <=0 	
-		g.translate(camera.getX(), camera.getY()); //Start of camera
-		
+
+	public void relocateCam(Graphics g) {
+		camera.zoom(g, camera.getZoom());// Crasher om verdien <=0
+		g.translate(camera.getX(), camera.getY()); // Start of camera
+
 		g.drawImage(subMapPic, 0, 0);
-		for(Car car: cars){
+		for (Car car : cars) {
 			car.render(g);
 		}
-		g.drawImage(mapPic,0,0);
-		
-		g.translate(-camera.getX(), -camera.getY()); //End of camera
-		camera.zoom(g, 1/camera.getZoom());
+		g.drawImage(mapPic, 0, 0);
+
+		g.translate(-camera.getX(), -camera.getY()); // End of camera
+		camera.zoom(g, 1 / camera.getZoom());
 	}
-	public void initVariables(){
-		
+
+	public void initVariables() {
+
 		cars = new ArrayList<Car>();
-		
+
 		raceStarted = false;
 		countdownStarted = false;
-		
+
 		startGoSignal = true;
-		
+
 		keyboardPlayerOne = false;
 		keyboardPlayerOne = false;
-		
+
 		try {
 			subMapPic = new Image("Maps/course1sub.png");
 			mapPic = new Image("Maps/course1.png");
@@ -380,46 +333,17 @@ public class GameCore extends BasicGameState {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		font = new Font("Verdana", Font.BOLD, 20);
 		ttf = new TrueTypeFont(font, true);
-		
+
 		screenWidth = Application.screenSize.width;
 		screenHeight = Application.screenSize.height;
-		
-		headerSize = 30f;
-		textSize = 24f;
-		InputStream inputStream;
-		try {
-			inputStream = ResourceLoader.getResourceAsStream("Font/Orbitron-Regular.ttf");
-			Font awtFont1 = Font.createFont(Font.TRUETYPE_FONT, inputStream);
-			Font awtFont2;
 
-			awtFont1 = awtFont1.deriveFont(headerSize); // set font size
-			awtFont2 = awtFont1.deriveFont(textSize);
-			
-			scoreBoardHeader = new TrueTypeFont(awtFont1, true);
-			scoreBoardText = new TrueTypeFont(awtFont2, true);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 		
-		String path = "/Sprites/UI/";
-		try {
-			 results = new Image(path + "border.png");
-			 highscores = new Image(path + "border.png");
-		} catch (SlickException e) {
-			e.printStackTrace();
-		}
-		
-		resultPosX = 0-(results.getWidth() * 2);
-		highScorePosX = (float) (Application.screenSize.width + (results.getWidth() / 1.3));
-		resultPosY =  Application.screenSize.height/10;
-		highScorePosY = Application.screenSize.height/10;
-		
+
 	}
-	
+
 	public void setIP(String ip) {
 		IP = ip + "/furyracers";
 	}
--- game/src/main/java/com/github/fredrikzkl/furyracers/game/ScoreBoard.java
+++ game/src/main/java/com/github/fredrikzkl/furyracers/game/ScoreBoard.java
@@ -1,5 +1,159 @@
 package com.github.fredrikzkl.furyracers.game;
 
+import java.awt.Font;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.TrueTypeFont;
+import org.newdawn.slick.util.ResourceLoader;
+
+import com.github.fredrikzkl.furyracers.Application;
+
 public class ScoreBoard {
+
+	private Image results, highscores;
+	float resultPosX, resultPosY, highScorePosX, highScorePosY;
+
+	private boolean toMenuTimerSet, returnToMenuTimerDone;
+	private long toMenuRealTimer;
+	private String toMenuTimer = "";
+	private int timeToMenu = 15;
+
+	private TrueTypeFont scoreBoardHeader;
+	private TrueTypeFont scoreBoardText;
+	private float headerSize, textSize;
+	private int textTimer = 0;
+	int scoreBoardLength;
+
+	private Color headerColor = new Color(221, 0, 0);
+
+	public ArrayList<Car> cars;
+	public ArrayList<Player> players;
+
+	public ScoreBoard(List<Car> cars2, List<Player> players2) {
+
+		this.cars = (ArrayList<Car>) cars2;
+		this.players = (ArrayList<Player>) players2;
+
+		returnToMenuTimerDone = false;
+		headerSize = 30f;
+		textSize = 24f;
+
+		InputStream inputStream;
+		try {
+			inputStream = ResourceLoader.getResourceAsStream("Font/Orbitron-Regular.ttf");
+			Font awtFont1 = Font.createFont(Font.TRUETYPE_FONT, inputStream);
+			Font awtFont2;
+
+			awtFont1 = awtFont1.deriveFont(headerSize); // set font size
+			awtFont2 = awtFont1.deriveFont(textSize);
+
+			scoreBoardHeader = new TrueTypeFont(awtFont1, true);
+			scoreBoardText = new TrueTypeFont(awtFont2, true);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		String path = "/Sprites/UI/";
+		try {
+			results = new Image(path + "border.png");
+			highscores = new Image(path + "border.png");
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+
+		resultPosX = 0 - (results.getWidth() * 2);
+		highScorePosX = (float) (Application.screenSize.width + (results.getWidth() / 1.3));
+		resultPosY = Application.screenSize.height / 10;
+		highScorePosY = Application.screenSize.height / 10;
+	}
+
+	public void drawScoreBoard() {
+		float speed = 1;
+		float maxX = Application.screenSize.width;
+		float midWay = maxX / 2;
+		float marginX = midWay / 5;
+		float scalingValue = (marginX * 3) / results.getWidth();
+
+		results.draw(resultPosX - results.getWidth(), resultPosY, scalingValue);
+		highscores.draw(highScorePosX + highscores.getWidth(), highScorePosY, scalingValue);
+
+		if (resultPosX < marginX * 3.5 || highScorePosX > (midWay - (marginX * 1.3))) {
+			resultPosX += speed;
+			highScorePosX -= speed;
+		} else {
+			printScores(resultPosX - results.getWidth());
+			printHighScores(highScorePosX + results.getWidth());
+			printTimer(maxX);
+		}
+
+	}
+
+	private void printTimer(float x) {
+		if (!toMenuTimerSet) {
+			toMenuRealTimer = System.nanoTime();
+			toMenuTimerSet = true;
+		}
+		long currentTime = System.nanoTime();
+		long elapsed = TimeUnit.NANOSECONDS.toSeconds(currentTime - toMenuRealTimer);
+		toMenuTimer = "" + (timeToMenu - elapsed);
+		scoreBoardHeader.drawString(x - (headerSize * 1.5f), Application.screenSize.height - headerSize, toMenuTimer);
+
+		if (timeToMenu - elapsed == 0) {
+			returnToMenuTimerDone = true;
+		}
+	}
+
+	private void printHighScores(float borderX) {
+		float headerPosX = borderX + 20;
+		float headerPosY = resultPosY * 1.4f;
+		scoreBoardHeader.drawString(headerPosX, headerPosY, "High Scores:", headerColor);
+
+	}
+
+	private void printScores(float borderX) {
+		float headerPosX = borderX + 20;
+		float headerPosY = resultPosY * 1.4f;
+
+		scoreBoardHeader.drawString(headerPosX, headerPosY, "Results:", headerColor);
+		scoreBoardLength = (int) headerSize;
+		
+		ArrayList<Car> sortedCars = cars;
+		Collections.sort(sortedCars);
+
+		for (int i = sortedCars.size() - 1; i >= 0; i--) {
+			scoreBoardText.drawString(headerPosX, headerPosY + scoreBoardLength,
+					"Player " + sortedCars.get(i).getPlayerNr() + ": " + sortedCars.get(i).getTimeElapsed() + " Score: "
+							+ "+" + (i + 1));
+			scoreBoardLength += textSize;
+		}
+
+		scoreBoardHeader.drawString(headerPosX, headerPosY + headerSize + scoreBoardLength, "Total Score:",
+				headerColor);
+		scoreBoardLength += headerSize * 2;
+
+		ArrayList<Player> sortedPlayers = players;
+		Collections.sort(sortedPlayers);
+
+		for (int i = 0; i < sortedPlayers.size(); i++) {
+			scoreBoardText.drawString(headerPosX, headerPosY + scoreBoardLength,
+					sortedPlayers.get(i).getId() + ": " + sortedPlayers.get(i).getScore());
+			scoreBoardLength += textSize;
+
+		}
+
+		textTimer++;
+	}
+
+	public boolean isReturnToMenuTimerDone() {
+		return returnToMenuTimerDone;
+	}
 
 }
